2b5ee68d7c421e8ce2eaf984b455dc2dae588798
Pablo <pablo268la@gmail.com>
2023-02-18 23:41:50 +0100

    Catch in all backend controllers

--- restapi/controladores/equiposController.ts
+++ restapi/controladores/equiposController.ts
@@ -2,9 +2,19 @@ import { RequestHandler } from "express";
 import { modeloEquipo } from "../model/equipo";
 
 export const getEquipos: RequestHandler = async (req, res) => {
-	res.json(await modeloEquipo.find());
+	try {
+		res.status(200).json(await modeloEquipo.find());
+	} catch (error) {
+		res.status(500).json(error);
+	}
 };
 
 export const getEquipo: RequestHandler = async (req, res) => {
-	res.json(await modeloEquipo.findOne({ _id: req.params.idEquipo }));
+	try {
+		res
+			.status(200)
+			.json(await modeloEquipo.findOne({ _id: req.params.idEquipo }));
+	} catch (error) {
+		res.status(500).json(error);
+	}
 };
