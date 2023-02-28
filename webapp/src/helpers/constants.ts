9227836bbc45e84b9cf8cdcbadd3f8b74d4fe479
Pablo <pablo268la@gmail.com>
2023-02-28 21:59:36 +0100

    C

--- webapp/src/helpers/constants.ts
+++ webapp/src/helpers/constants.ts
@@ -1,2 +1,2 @@
-//const apiEndPoint = "http://" + process.env.REACT_APP_API_URI + ":5000";
+//export const apiEndPoint = "http://" + process.env.REACT_APP_API_URI + ":5000";
 export const apiEndPoint = "http://localhost:5000";
