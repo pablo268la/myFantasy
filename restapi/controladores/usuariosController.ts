2b81e8da74b63ef6bd67cb2d2e7518c366b6c48a
Pablo <pablo268la@gmail.com>
2023-05-21 19:52:48 +0200

    :)

--- restapi/controladores/usuariosController.ts
+++ restapi/controladores/usuariosController.ts
@@ -1,4 +1,4 @@
-import * as bcrypt from "bcrypt";
+import * as bcrypt from "bcryptjs";
 import { RequestHandler } from "express";
 import * as jwt from "jsonwebtoken";
 import * as UUID from "uuid";
